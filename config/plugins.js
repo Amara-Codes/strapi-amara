72cd193892d17f64425e811194a88aea32fa68d4
Roberto Barbagallo <developer@greenleafstudio.it>
2025-11-23 10:50:00 +0700

    try

--- config/plugins.js
+++ config/plugins.js
@@ -25,17 +25,18 @@ module.exports = ({ env }) => ({
         default: {
           // 💡 PUNTO CHIAVE: Uso della proprietà 'url' invece di 'connection'
           // per accettare l'URI completo di Redis.
-          url: 'redis://default:IiqeAZIqusWnQMfcDvPeZPictwdIDkmT@redis.railway.internal:6379', 
+         
 
           // Se per qualche motivo il provider non supporta 'url' direttamente:
-          /*
+          
           connection: {
-            host: env("REDIS_HOST", "127.0.0.1"), 
-            port: env.int("REDIS_PORT", 6379),
-            db: env.int("REDIS_DB", 0),
-            password: env("REDIS_PASSWORD"), 
+           url: env("REDIS_URL"), 
+            //host: env("REDIS_HOST", "127.0.0.1"), 
+            //port: env.int("REDIS_PORT", 6379),
+            //db: env.int("REDIS_DB", 0),
+            //password: env("REDIS_PASSWORD"), 
           },
-          */
+          
           settings: {
             debug: false,
           },
